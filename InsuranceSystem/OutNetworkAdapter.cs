a9f33640d7f29b68b121ecc3d80429c3ac15753b
Sumner Cotton <scotton@css.edu>
2024-10-06 15:09:10 -0500

    Assignment 5 push

---
+++ InsuranceSystem/OutNetworkAdapter.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Linq;
+
+namespace Unit5_AdapterPatternPatient_Blazor.InsuranceSystem
+{
+    public class OutNetworkAdapter : InsuranceInterface
+    {
+        OutNetworkPatient patient;
+        public OutNetworkAdapter(string newName, int newPolicyNumber)
+        { 
+            patient = new OutNetworkPatient(newName, newPolicyNumber);
+        }
+        public decimal CoverageAmount(int ProcedureID, decimal ProcedureCost)
+        {
+            decimal coveragePercent = patient.CoveragePercent(ProcedureCost);    
+            return(coveragePercent* ProcedureCost);
+        }
+
+        public string getPatientName()
+        {
+            return patient.getPatientName();
+           
+        }
+
+        public string getPolicyNumber()
+        {
+            return patient.PolicyNumber.ToString();
+        }
+
+        public bool IsCovered(string patientName, string policyNumber)
+        {
+            return true;
+            //string covered = patient.IsCovered(patientName, policyNumber);
+        }
+    }
+}
